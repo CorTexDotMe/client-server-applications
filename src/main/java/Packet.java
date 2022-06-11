f617f373aea3ae9e0e14bd2fa4ea30330ecaaff4
CorTexDotMe <danylo.nechyporchuk@gmail.com>
2022-06-11 13:08:21 +0300

    Checksum(CRC16) for both Packet and PacketToSend
    7. PacketToSend create a checksum
    6. Packet use the checksum to check whether packet is correct
    CRC16 as checksum
    
    Still no encoding.

--- src/main/java/Packet.java
+++ src/main/java/Packet.java
@@ -1,11 +1,17 @@
+import helpers.CRC16;
+
 import java.nio.ByteBuffer;
 
 public class Packet {
+    public static final int BYTES_AMOUNT_FOR_FIRST_CHECKSUM = 14;
+    public static final int BYTES_OFFSET_FOR_FIRST_CHECKSUM = 0;
+    public static final int BYTES_OFFSET_FOR_SECOND_CHECKSUM = 16;
     public static final byte bMagic = 0x13;
+
     private byte bSrc;
     private long bPktId;
     private int wLen;
-    private short wCrc16;
+    private short wCrc16_first;
     private Message bMsg;
     private short wCrc16_second;
 
@@ -14,11 +20,26 @@ public class Packet {
 
         if (buffer.get() != bMagic)
             throw new IllegalArgumentException("No start byte in byte array");
+
         bSrc = buffer.get();
         bPktId = buffer.getLong();
         wLen = buffer.getInt();
-        wCrc16 = buffer.getShort();
+        wCrc16_first = buffer.getShort();
+
+        if (wrongChecksum(wCrc16_first, buffer, BYTES_OFFSET_FOR_FIRST_CHECKSUM, BYTES_AMOUNT_FOR_FIRST_CHECKSUM))
+            throw new IllegalArgumentException("Wrong first checksum");
+
         bMsg = new Message(buffer, wLen);
         wCrc16_second = buffer.getShort();
+
+        if (wrongChecksum(wCrc16_second, buffer, BYTES_OFFSET_FOR_SECOND_CHECKSUM, wLen))
+            throw new IllegalArgumentException("Wrong second checksum");
+    }
+
+    private boolean wrongChecksum(short expectedChecksum, ByteBuffer buffer,
+                                  int packetOffset, int packetToCheckLength) {
+        byte[] bytesForChecksum = new byte[packetToCheckLength];
+        buffer.get(bytesForChecksum, packetOffset, packetToCheckLength);
+        return expectedChecksum != CRC16.getCRC16(bytesForChecksum);
     }
 }
--- src/main/java/PacketToSend.java
+++ src/main/java/PacketToSend.java
@@ -1,24 +1,32 @@
+import helpers.CRC16;
+
 import java.nio.ByteBuffer;
 
 public class PacketToSend {
-    private final int packetLength = 18;
+    private final int packetLengthWithoutMessage = 18;
     private byte[] packet;
 
     public PacketToSend(byte bSrc, long bPktId, int wLen, Message bMsg) {
-        packet = new byte[packetLength + wLen];
+        packet = new byte[packetLengthWithoutMessage + wLen];
         ByteBuffer buffer = ByteBuffer.wrap(packet);
         buffer.put(Packet.bMagic);
         buffer.put(bSrc);
         buffer.putLong(bPktId);
         buffer.putInt(wLen);
 
-        //buffer.putShort(wCrc16);
+        byte[] bytesForFirstChecksum = new byte[Packet.BYTES_AMOUNT_FOR_FIRST_CHECKSUM];
+        buffer.get(bytesForFirstChecksum,
+                Packet.BYTES_OFFSET_FOR_FIRST_CHECKSUM,
+                Packet.BYTES_AMOUNT_FOR_FIRST_CHECKSUM);
+        buffer.putShort(CRC16.getCRC16(bytesForFirstChecksum));
 
         buffer.putInt(bMsg.getcType());
         buffer.putInt(bMsg.getbUserId());
         buffer.put(bMsg.getMessage());
 
-        //buffer.putShort(wCrc16_second);
+        byte[] messageByteArray = new byte[wLen];
+        buffer.get(messageByteArray, Packet.BYTES_OFFSET_FOR_SECOND_CHECKSUM, wLen);
+        buffer.putShort(CRC16.getCRC16(messageByteArray));
     }
 
     public byte[] getPacket() {
