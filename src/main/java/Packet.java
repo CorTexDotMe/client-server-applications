21b2f33e4c951c94c4c48ae1b29eadf85802b07c
CorTexDotMe <danylo.nechyporchuk@gmail.com>
2022-06-12 10:54:57 +0300

    Encryption and decryption
    Packet encrypt message when created
    Packet decrypt message when received
    
    I suppose it won't work. Need some tests.

--- src/main/java/Packet.java
+++ src/main/java/Packet.java
@@ -1,6 +1,11 @@
 import helpers.CRC16;
 
+import javax.crypto.*;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
 import java.nio.ByteBuffer;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
 
 public class Packet {
     private static final int BYTES_AMOUNT_FOR_FIRST_CHECKSUM = 14;
@@ -15,10 +20,16 @@ public class Packet {
     private short wCrc16_first;
     private Message bMsg;
     private short wCrc16_second;
-
     private byte[] packet;
 
-    public Packet(byte bSrc, long bPktId, int wLen, Message bMsg) {
+    private Key key;
+    private Cipher cipher;
+
+    public Packet(byte bSrc, long bPktId, int wLen, Message bMsg)
+            throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException,
+            ShortBufferException, IllegalBlockSizeException, BadPaddingException {
+        cipherInit();
+
         this.packet = new byte[PACKET_LENGTH_WITHOUT_MESSAGE + wLen];
         ByteBuffer buffer = ByteBuffer.wrap(this.packet);
         buffer.put(bMagic);
@@ -32,16 +43,23 @@ public class Packet {
                 BYTES_AMOUNT_FOR_FIRST_CHECKSUM);
         buffer.putShort(CRC16.getCRC16(bytesForFirstChecksum));
 
+
         buffer.putInt(bMsg.getcType());
         buffer.putInt(bMsg.getbUserId());
         buffer.put(bMsg.getMessage());
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        cipher.doFinal(packet, BYTES_OFFSET_FOR_SECOND_CHECKSUM, wLen, packet, BYTES_OFFSET_FOR_SECOND_CHECKSUM);
 
         byte[] messageByteArray = new byte[wLen];
-        buffer.get(messageByteArray, Packet.BYTES_OFFSET_FOR_SECOND_CHECKSUM, wLen);
+        buffer.get(messageByteArray, BYTES_OFFSET_FOR_SECOND_CHECKSUM, wLen);
         buffer.putShort(CRC16.getCRC16(messageByteArray));
     }
 
-    public Packet(byte[] bytes) {
+    public Packet(byte[] bytes)
+            throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException,
+            ShortBufferException, IllegalBlockSizeException, BadPaddingException {
+        cipherInit();
+
         this.packet = bytes;
         ByteBuffer buffer = ByteBuffer.wrap(bytes);
 
@@ -56,15 +74,31 @@ public class Packet {
         if (wrongChecksum(this.wCrc16_first, buffer, BYTES_OFFSET_FOR_FIRST_CHECKSUM, BYTES_AMOUNT_FOR_FIRST_CHECKSUM))
             throw new IllegalArgumentException("Wrong first checksum");
 
-        this.bMsg = new Message(buffer, this.wLen);
-        this.wCrc16_second = buffer.getShort();
+        byte[] message = new byte[this.wLen];
+        this.cipher.init(Cipher.DECRYPT_MODE, this.key);
+        this.cipher.doFinal(this.packet, BYTES_OFFSET_FOR_SECOND_CHECKSUM, this.wLen, message, 0);
+        this.bMsg = new Message(ByteBuffer.wrap(message), this.wLen);
 
+        int secondChecksumIndex = BYTES_OFFSET_FOR_SECOND_CHECKSUM + wLen;
+        this.wCrc16_second = buffer.getShort(secondChecksumIndex);
         if (wrongChecksum(this.wCrc16_second, buffer, BYTES_OFFSET_FOR_SECOND_CHECKSUM, this.wLen))
             throw new IllegalArgumentException("Wrong second checksum");
     }
 
     public byte[] getPacket() {
         return packet;
+    }
+
+    private void cipherInit() throws NoSuchAlgorithmException, NoSuchPaddingException {
+        this.key = generateKey();
+        this.cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+    }
+
+    private static Key generateKey() throws NoSuchAlgorithmException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        int keyBitSize = 256;
+        keyGenerator.init(keyBitSize);
+        return keyGenerator.generateKey();
     }
 
     private boolean wrongChecksum(short expectedChecksum, ByteBuffer buffer,
