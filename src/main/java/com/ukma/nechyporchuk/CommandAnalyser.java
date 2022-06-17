57e334da0a55c8b9cf7bcd95e083dfd692bb549e
CorTexDotMe <danylo.nechyporchuk@gmail.com>
2022-06-17 22:14:32 +0300

    Command in int cType now stores as bytes in integer value
    To represent different commands in one int value different bytes do the trick. Certain bytes now express idea of which command was in this integer value.
    
    Plus some comments.

---
+++ src/main/java/com/ukma/nechyporchuk/CommandAnalyser.java
@@ -0,0 +1,47 @@
+package com.ukma.nechyporchuk;
+
+/**
+ * Because commands are sent in packet as int value, it needs to be analyzed
+ * Integer can represent different commands with different bytes.
+ *<p>
+ * At first, first and second bits represent GROUP and ITEM.
+ * If first bit is set, command will be operating with GROUP
+ *
+ * Then next bits represent different command(GET, REMOVE, ADD, CREATE, etc.)
+ *
+ * @author Danylo Nechyporchuk
+ */
+public class CommandAnalyser {
+    public static final int
+            GROUP = 1,
+            ITEM = 2;
+
+    public static final int
+            GET = 4,
+            REMOVE = 8,
+            ADD = 16,
+            CREATE = 32,
+            SET_PRICE = 64;
+
+    public static final int
+            ITEM_GET        = ITEM ^ GET,
+            ITEM_REMOVE     = ITEM ^ REMOVE,
+            ITEM_ADD        = ITEM ^ ADD,
+            ITEM_CREATE     = ITEM ^ CREATE,
+            ITEM_SET_PRICE  = ITEM ^ SET_PRICE;
+
+    public static final int
+            GROUP_CREATE = GROUP ^ CREATE;
+
+    static boolean isItemCommand(int cType) {
+        return (cType & ITEM) == ITEM;
+    }
+
+    static int getCommand(int cType, boolean isItemCommand) {
+        return cType ^ (isItemCommand ? ITEM : GROUP);
+    }
+
+    static int getCommand(int cType) {
+        return cType ^ (isItemCommand(cType) ? ITEM : GROUP);
+    }
+}
